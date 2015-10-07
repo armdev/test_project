52b92a197c7251620d9b6a9b3dc73e0ebf252a87
IEUser <IEUser@IE11Win7>
2015-10-07 04:11:24 -0700

    test project

---
+++ public_web/src/main/java/com/web/jsf/utils/PostBody.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.web.jsf.utils;
+
+/**
+ *
+ * @author IEUser
+ */
+public class PostBody {
+
+    private String title;
+    private String url;
+    private String image;
+    private String keywords;
+    private String description;
+
+    public PostBody() {
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
